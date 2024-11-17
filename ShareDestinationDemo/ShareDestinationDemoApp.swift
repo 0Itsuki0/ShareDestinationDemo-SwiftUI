5a3b392c11704ca517cdbf47dd0bdca560755846
itsuki <itsuki.enjyoy@gmail.com>
2024-11-17 12:53:34 +0900

    Initial Commit

---
+++ ShareDestinationDemo/ShareDestinationDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShareDestinationDemoApp.swift
+//  ShareDestinationDemo
+//
+//  Created by Itsuki on 2024/11/17.
+//
+
+import SwiftUI
+
+@main
+struct ShareDestinationDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
